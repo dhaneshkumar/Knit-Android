d8d8eb67c9e71b8c5a0c3bbca35fb02ddaa2f8fe
Dhanesh Kumar <dhaneshkr26@gmail.com>
2015-01-11 23:26:46 -0800

    outbox gui updated again

--- splashScreen/src/main/java/trumplabs/schoolapp/Application.java
+++ splashScreen/src/main/java/trumplabs/schoolapp/Application.java
@@ -34,7 +34,7 @@ public class Application extends android.app.Application {
 	Parse.initialize(this, Config.APP_ID, Config.CLIENT_KEY); 
 	Application.context = getApplicationContext();
 
-	// Specify an Activity to handle all pushes by default.
+	// Specify an Activity to handle all pushes by   default.
 	//PushService.setDefaultPushCallback(this, trumplabs.schoolapp.MainActivity.class);
 	PushService.setDefaultPushCallback(this, trumplabs.schoolapp.MainActivity.class,getResources().getIdentifier("notifications", "drawable", getPackageName()));
 	ParseInstallation.getCurrentInstallation().saveEventually();
