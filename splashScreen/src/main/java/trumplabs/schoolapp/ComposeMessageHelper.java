c2ed3e6662ac787b1304dee76a5b386761cc351b
Ashish Sonone <ashson07@gmail.com>
2015-07-27 00:01:55 +0530

    now for multicast : need to send data only once in a function call

--- splashScreen/src/main/java/trumplabs/schoolapp/ComposeMessageHelper.java
+++ splashScreen/src/main/java/trumplabs/schoolapp/ComposeMessageHelper.java
@@ -15,9 +15,11 @@ import com.parse.SaveCallback;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import BackGroundProcesses.SendPendingMessages;
 import library.UtilString;
@@ -142,6 +144,9 @@ public class ComposeMessageHelper {
 
     private void sendTxtMsgtoSubscribers(final String typedtxt) {
         final List<ParseObject> messagesToSend = new ArrayList<>();
+
+        Long batchId = Calendar.getInstance().getTimeInMillis();
+
         for(List<String> cls : selectedClassList){
             final ParseObject sentMsg = new ParseObject(Constants.SENT_MESSAGES_TABLE);
             sentMsg.put("Creator", sender);
@@ -152,6 +157,7 @@ public class ComposeMessageHelper {
             sentMsg.put("senderId", userId);
             sentMsg.put("userId", userId);
             sentMsg.put("pending", true);
+            sentMsg.put(Constants.BATCH_ID, batchId);
 
             ComposeMessage.typedmsg.setText(""); //for reuse
 
@@ -188,9 +194,6 @@ public class ComposeMessageHelper {
                 String objectId = (String) result.get("messageId");
 
                 if (createdAt != null) {
-                    //updating local time
-                    SessionManager sm = new SessionManager(Application.getAppContext());
-                    sm.setCurrentTime(createdAt);
 
                     //update msg and pin
                     msg.put("objectId", objectId);
@@ -253,9 +256,128 @@ public class ComposeMessageHelper {
         }
     }
 
+    /*always called in thread for background sending
+     return values
+        0 : success,
+        100 : network error
+        ParseException.INVALID_SESSION_TOKEN(209) : invalid session token
+        -1 : failure due to other error(won't happen usually, hence safe to ignore and continue with other pending messsages)
+    */
+    public static int sendMultiTextMessageCloud(final List<ParseObject> batch){
+        if(!Utility.isInternetExistWithoutPopup()){
+            Log.d(SendPendingMessages.LOGTAG, "send text cloud : saving cloud call when offline");
+            return 100; //not connected to internet
+        }
+
+        ParseObject master = batch.get(0);
+
+        List<String> classcodes = new ArrayList<>();
+        List<String> classnames = new ArrayList<>();
+        for(int i=0; i<batch.size(); i++){
+            classcodes.add(batch.get(i).getString("code"));
+            classnames.add(batch.get(i).getString("name"));
+        }
+
+        //if live, then only show "sent" toast
+        //sending message using parse cloud function
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put("classcode", classcodes);
+        params.put("classname", classnames);
+        params.put("message", master.getString("title"));
+
+        try{
+            HashMap result = ParseCloud.callFunction("sendMultiTextMessage", params);
+            if (result != null) {
+                int retVal = 0; //success
+                List<Date> createdAtList = (List<Date>) result.get("createdAt");
+                List<String> objectIdList = (List<String>) result.get("messageId");
+
+                if(createdAtList == null || objectIdList == null){
+                    return -1; //unexpected error
+                }
+
+                Log.d(SendPendingMessages.LOGTAG, "sendMultiTextMessage response objectIdList=" + objectIdList);
+
+                List<ParseObject> failedMessages = new ArrayList<>();
+
+                for(int i=0; i<createdAtList.size(); i++){
+                    Date createdAt = createdAtList.get(i);
+                    String objectId = objectIdList.get(i);
+
+                    ParseObject msg = batch.get(i);
+
+                    if(createdAt == null){
+                        failedMessages.add(msg);
+                    }
+
+                    //update msg and pin
+                    msg.put("objectId", objectId);
+                    msg.put("pending", false);
+                    msg.put("creationTime", createdAt);
+
+                    msg.pinInBackground();
+                }
+
+                if(failedMessages.size() == batch.size()){
+                    retVal = 200; //only if all fail
+                    //class has been deleted
+                    //error that class does not exist
+                }
+
+                if(failedMessages.size() > 0) {
+                    //first update created groups of user
+                    Log.d(SendPendingMessages.LOGTAG, "DELETED #classes=" + failedMessages.size());
+
+                    List<List<String>> updatedCreatedGroups = (List<List<String>>) result.get(Constants.CREATED_GROUPS);
+                    try {
+                        ParseUser user = ParseUser.getCurrentUser();
+                        if (user != null && updatedCreatedGroups != null) {
+                            user.put(Constants.CREATED_GROUPS, updatedCreatedGroups);
+                            user.pin();
+                        }
+                    } catch (ParseException err) {
+                        err.printStackTrace();
+                    }
+
+                    //Notify created classrooms adapter
+                    //TODO Classrooms.refreshCreatedClassrooms(msg.getString(Constants.GroupDetails.CODE));
+
+                    //unpin the message,
+                    ParseObject.unpinAll(failedMessages);
+                    //delete the message from lists
+                    if (SendMessage.groupDetails != null) {
+                        SendMessage.groupDetails.remove(failedMessages);
+                    }
+                    if (Outbox.groupDetails != null) {
+                        Outbox.groupDetails.remove(failedMessages);
+                    }
+                }
+
+                //notify outbox and class page adapter
+                SendMessage.notifyAdapter();
+                Outbox.notifyAdapter();
+                return retVal; //success
+            }
+            return -1; //unexpected error
+        }
+        catch (ParseException e){
+            if(Utility.LogoutUtility.checkAndHandleInvalidSession(e)){
+                return ParseException.INVALID_SESSION_TOKEN;
+            }
+            else if(e.getCode() == ParseException.CONNECTION_FAILED){
+                return 100; //network error
+            }
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
     // Send Image Pic
     private void sendPic(String filepath, String txtmsg){
         final List<ParseObject> messagesToSend = new ArrayList<>();
+
+        Long batchId = Calendar.getInstance().getTimeInMillis();
+
         for(List<String> cls : selectedClassList) {
             if (filepath == null) return;
 
@@ -269,6 +391,7 @@ public class ComposeMessageHelper {
             sentMsg.put("senderId", userId);
             sentMsg.put("userId", userId);
             sentMsg.put("pending", true);
+            sentMsg.put(Constants.BATCH_ID, batchId);
 
             int slashindex = filepath.lastIndexOf("/");
             final String fileName = filepath.substring(slashindex + 1);// image file //
@@ -315,7 +438,7 @@ public class ComposeMessageHelper {
 
         final ParseFile file = new ParseFile(imageName, data);
 
-        Log.d(SendPendingMessages.LOGTAG, "sendPicMessageCloud : data size=" + data.length + " bytes, name="+imageName + ", old=" + oldName);
+        Log.d(SendPendingMessages.LOGTAG, "sendPicMessageCloud : data size=" + data.length + " bytes, name=" + imageName + ", old=" + oldName);
 
         try {
             file.save();
@@ -400,6 +523,154 @@ public class ComposeMessageHelper {
             esave.printStackTrace();
             return -1;
             //Utility.toast("Sorry, sending failed now. We'll send it next time you're online");
+        }
+    }
+
+    /*always called in thread for background sending
+    return values
+       0 : success,
+       100 : network error
+       ParseException.INVALID_SESSION_TOKEN(209) : invalid session token
+       -1 : failure due to other error(won't happen usually, hence safe to ignore and continue with other pending messsages)
+   */
+    public static int sendMultiPicMessageCloud(final List<ParseObject> batch){
+        if(!Utility.isInternetExistWithoutPopup()){
+            Log.d(SendPendingMessages.LOGTAG, "send text cloud : saving cloud call when offline");
+            return 100; //not connected to internet
+        }
+
+        ParseObject master = batch.get(0);
+
+        List<String> classcodes = new ArrayList<>();
+        List<String> classnames = new ArrayList<>();
+        for(int i=0; i<batch.size(); i++){
+            classcodes.add(batch.get(i).getString("code"));
+            classnames.add(batch.get(i).getString("name"));
+        }
+
+        /* image file work */
+        //don't have attachement, objectid
+        //update creationTime, pending
+        String imageName = null;
+        if (master.containsKey("attachment_name"))
+            imageName = master.getString("attachment_name");
+
+        if (imageName == null) return -1; //no attachment
+
+        byte[] data = null;
+        try {
+            RandomAccessFile f = new RandomAccessFile(Utility.getWorkingAppDir() + "/media/" + imageName, "r");
+            data = new byte[(int) f.length()];
+            f.read(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1; //io exception
+        }
+
+        String oldName = imageName;
+        imageName = imageName.replaceAll("[^a-zA-Z0-9_\\.]", "");
+        imageName = "i" + imageName;
+
+        final ParseFile file = new ParseFile(imageName, data);
+
+        Log.d(SendPendingMessages.LOGTAG, "sendPicMessageCloud : data size=" + data.length + " bytes, name=" + imageName + ", old=" + oldName);
+
+        try{
+            file.save();
+            Log.d(SendPendingMessages.LOGTAG, "sendPicMessageCloud : file save success");
+
+            //sending message using parse cloud function
+            HashMap<String, Object> params = new HashMap<String, Object>();
+            params.put("classcode", classcodes);
+            params.put("classname", classnames);
+            params.put("message", master.getString("title"));
+            params.put("filename", master.getString("attachment_name"));
+            params.put("parsefile", file);
+
+            HashMap result = ParseCloud.callFunction("sendMultiPhotoTextMessage", params);
+            if (result != null) {
+                int retVal = 0; //success
+                List<Date> createdAtList = (List<Date>) result.get("createdAt");
+                List<String> objectIdList = (List<String>) result.get("messageId");
+
+                if(createdAtList == null || objectIdList == null){
+                    return -1; //unexpected error
+                }
+
+                Log.d(SendPendingMessages.LOGTAG, "sendMultiTextMessage response objectIdList=" + objectIdList);
+
+                List<ParseObject> failedMessages = new ArrayList<>();
+
+                for(int i=0; i<createdAtList.size(); i++){
+                    Date createdAt = createdAtList.get(i);
+                    String objectId = objectIdList.get(i);
+
+                    ParseObject msg = batch.get(i);
+
+                    if(createdAt == null){
+                        failedMessages.add(msg);
+                    }
+
+                    //update msg and pin
+                    msg.put("objectId", objectId);
+                    msg.put("pending", false);
+                    msg.put("creationTime", createdAt);
+                    msg.put("attachment", file);
+
+                    msg.pinInBackground();
+                }
+
+                if(failedMessages.size() == batch.size()){
+                    retVal = 200; //only if all fail
+                    //class has been deleted
+                    //error that class does not exist
+                }
+
+                if(failedMessages.size() > 0) {
+                    //first update created groups of user
+                    Log.d(SendPendingMessages.LOGTAG, "DELETED #classes=" + failedMessages.size());
+
+                    List<List<String>> updatedCreatedGroups = (List<List<String>>) result.get(Constants.CREATED_GROUPS);
+                    try {
+                        ParseUser user = ParseUser.getCurrentUser();
+                        if (user != null && updatedCreatedGroups != null) {
+                            user.put(Constants.CREATED_GROUPS, updatedCreatedGroups);
+                            user.pin();
+                        }
+                    } catch (ParseException err) {
+                        err.printStackTrace();
+                    }
+
+                    //Notify created classrooms adapter
+                    //TODO Classrooms.refreshCreatedClassrooms(msg.getString(Constants.GroupDetails.CODE));
+
+                    //unpin the message,
+                    ParseObject.unpinAll(failedMessages);
+                    //delete the message from lists
+                    if (SendMessage.groupDetails != null) {
+                        SendMessage.groupDetails.remove(failedMessages);
+                    }
+                    if (Outbox.groupDetails != null) {
+                        Outbox.groupDetails.remove(failedMessages);
+                    }
+                }
+
+                //notify outbox and class page adapter
+                SendMessage.notifyAdapter();
+                Outbox.notifyAdapter();
+                return retVal; //success
+            }
+            return -1; //unexpected error
+        }
+        catch (ParseException e){
+            if(Utility.LogoutUtility.checkAndHandleInvalidSession(e)){
+                return ParseException.INVALID_SESSION_TOKEN;
+            }
+            else if(e.getCode() == ParseException.CONNECTION_FAILED){
+                return 100; //network error
+            }
+            e.printStackTrace();
+            return -1;
         }
     }
 }
