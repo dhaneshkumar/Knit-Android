8f8be03cdf0424cd3c3a65bbb213247340b97917
Ashish Sonone <ashson07@gmail.com>
2015-08-27 01:11:57 +0530

    msg status{pending, sent to server, delivered, seen}; user status{online(bool), lastOnline(time)} TODO : add notification support

---
+++ splashScreen/src/main/java/chat/ChatConfig.java
@@ -0,0 +1,12 @@
+package chat;
+
+/**
+ * Created by ashish on 26/8/15.
+ */
+public class ChatConfig {
+    public static final String NON_TEACHER = "non_teacher";
+    public static final String TEACHER = "teacher";
+
+    public static final String MSG_TS_FORMAT = "dd/MM/yyyy hh:mm:ss a";
+    public static final String LAST_SEEN_FORMAT = "dd/MM hh:mm a";
+}
